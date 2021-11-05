2f8e5cfcf0e90cdae00ac7afe62720b555915dd4
barremans <bmcbarre@gmail.com>
2021-11-05 10:49:01 +0100

    Create wifi_credentials.h

---
+++ code/wifi_credentials.h
@@ -0,0 +1,12 @@
+#ifndef WIFI_CREDENTIALS_H
+#define WIFI_CREDENTIALS_H
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+
+#define MY_SSID "***********"
+#define MY_PASS "************"
+#define APNAME "ESP32 POWER"
+
+#endif
